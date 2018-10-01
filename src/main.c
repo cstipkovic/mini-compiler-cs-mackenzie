b4f46f31dcfa256a079ef8cf4ebbe9274bda63fb
Allan Gustavo Fernandes <allan@romanoff.local>
2018-10-01 15:40:50 -0300

    Correcao

--- src/main.c
+++ src/main.c
@@ -86,7 +86,7 @@ int needValue(char aux[]){
 /* Lê o arquivo com o código proposto */
 void readFile(char text[]) {
 	FILE *inputFile;
-	inputFile = fopen("../test/test-algC.txt", "r");
+	inputFile = fopen("./test/test-algC.txt", "r");
 	char line[400];
 
 	if (inputFile) {
